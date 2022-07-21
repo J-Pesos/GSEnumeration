b48da591922fc4e4248b0faaa91c8bc2033a0078
Joaquin Magana <joaquinmmagana@gmail.com>
2022-07-20 18:00:40 -0700

    Update Visualization_v2.R

--- Visualization_v2.R
+++ Visualization_v2.R
@@ -2,13 +2,14 @@ library(dplyr)
 library(tidyverse)
 library(ggplot2)
 library(rstudioapi)
-library(ggbeeswarm)
+library(beeswarm)
 
 ###Set working directory to source file location.
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 ###Read in data.
-data <- read.delim("syas_results.csv")
+data <- read.delim("fam36415_results.csv")
+data2 <- read.delim('familinx_summary.csv')
 
 ###Limit frequencies to only 0 GD relationships.
 mydata <- data[data$generation.depth == '0', ]
@@ -23,25 +24,54 @@ tally <- mydata_f %>%
   group_by(Indiv1, relation, .drop = FALSE) %>%
   tally()
 
-mydata_f %>%
-  group_by(Indiv1, relation, .drop = FALSE) %>%
-  summarize(freq = n()) %>%
-  ggplot(aes(x = relation,
-             y = freq)) +
-  scale_x_discrete(name ='Relationship',
-                   labels = c('Full Sib',
-                              'Full 1 Cuz',
-                              'Full 2 Cuz',
-                              'Half Sib',
-                              'Half 1 Cuz',
-                              'Half 2 Cuz',
-                              'Unk Sib',
-                              'Unk 1 Cuz')) +
-  #geom_beeswarm(cex = 3) +
-  geom_violin() +
-  theme_bw()
+beeswarm(tally$n ~ tally$relation,
+         col = c("#3FA0FF", "#6B8E23", "#F76D5E", '#AB82FF', '#00FFFF',
+                 '#BF3EFF', '#FF7F24', '#424242'),
+         spacing = 0.3,
+         pch = 20,
+         method = 'center',
+         corral = 'wrap',
+         labels = c('Full Sibling',
+                    'Full 1st Cousin',
+                    'Full 2nd Cousin',
+                    'Half Sibling',
+                    'Half 1st Cousin',
+                    'Half 2nd Cousin',
+                    'Unknown Sibling',
+                    'Unknown 1st Cousin'),
+         xlab = 'Relationship Type',
+         ylab = 'Number of Relations per Individual')
 
-ggsave("syas_violin.png")
+# first_degree_relations <- subset(tally, relation == 'full , 0 , 2' | relation == 'full , 0 , 4',
+#                                  select = c(relation, n))
+
+# composite_data <- first_degree_relations %>%
+#   composite(variables = c("relation", "n"), 
+#             name = "Score_comp",
+#             type = "sum",
+#             standardize = FALSE,
+#             missing.allowed = 0)
+
+#write.csv(first_degree_relations,'first_degree_relations.csv', row.names = FALSE)
+
+# mydata_f %>%
+#   group_by(Indiv1, relation, .drop = FALSE) %>%
+#   summarize(freq = n()) %>%
+#   ggplot(aes(x = relation,
+#              y = freq)) +
+#   scale_x_discrete(name ='Relationship',
+#                    labels = c('Full Sib',
+#                               'Full 1 Cuz',
+#                               'Full 2 Cuz',
+#                               'Half Sib',
+#                               'Half 1 Cuz',
+#                               'Half 2 Cuz',
+#                               'Unk Sib',
+#                               'Unk 1 Cuz')) +
+#   geom_violin() +
+#   theme_bw()
+
+#ggsave("syas_violin.png")
 
 # ###Create frequency data frame.
 # sel_col <- c('Indiv1', 'relation')
